bad9bcde7e6800d6d0eeaa60bf6e924ee77ecc96
Marc Herilala <hei.marc.3@gmail.com>
2024-03-12 21:34:22 +0300

    initialize spring project

---
+++ src/main/java/com/prog3/exam/Prog3Exam.java
@@ -0,0 +1,13 @@
+package com.prog3.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3Exam {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3Exam.class, args);
+    }
+
+}
---
+++ src/test/java/com/prog3/exam/AccountControllerIT.java
@@ -0,0 +1,12 @@
+package com.prog3.exam;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration()
+@WebAppConfiguration
+public class AccountControllerIT {
+}
