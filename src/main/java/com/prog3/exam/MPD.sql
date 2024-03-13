5d5367e0d1c9b17b9338f1df5241569a2c31d4fb
Artiana <hei.rojo.3@gmail.com>
2024-03-13 23:33:26 +0300

    update mpd.sql

--- src/main/java/com/prog3/exam/MPD.sql
+++ src/main/java/com/prog3/exam/MPD.sql
@@ -1,41 +1,46 @@
 CREATE  DATABASE digital_bank;
 
-CREATE TABLE account (
-    account_id serial PRIMARY KEY,
-    client_name varchar(150),
-    client_last_name varchar(150),
+CREATE  TABLE IF NOT EXISTS  account(
+    account_number bigint primary key,
+    client_name varchar(80),
+    client_last_name varchar(80),
     birthdate date,
     monthly_net_income decimal
 );
 
-
-CREATE TABLE sold (
-    id_sold serial PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS sold(
+    id_sold int primary key,
     balance decimal,
     loans decimal,
     loansInterest int,
     "date" date,
-    account_id integer,
-    FOREIGN KEY (account_id) REFERENCES account(account_id)
+    account_id bigint  REFERENCES account(account_number)
+
 );
 
-CREATE TABLE IF NOT EXISTS "transaction" (
-    id_transaction serial PRIMARY KEY,
-    "type" varchar(6) CHECK ("type" IN ('debit', 'credit')),
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE  TABLE  IF NOT EXISTS  "transaction"(
+    reference UUID DEFAULT uuid_generate_v4() primary key ,
+    "type" varchar(20) check (type='debit' or type='credit'),
     amount decimal,
     "date" date,
     reason varchar(20),
-    sender_account_id integer REFERENCES account(account_id),
-    recipient_account_id integer REFERENCES account(account_id)
+    sender_account bigint  REFERENCES account(account_number),
+    recipient_account bigint  REFERENCES account(account_number)
+
 );
 
-CREATE TABLE IF NOT EXISTS transfert (
+
+
+CREATE TABLE  IF NOT EXISTS transfert(
     reference UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    reason varchar(100),
+    reason varchar(50),
     amount decimal,
     effective_date date,
     registration_date date,
-    status varchar(7) CHECK (status IN ('cancel', 'pending', 'success'))
+    status varchar(30) check ( status='canceled' or status='pending' or status='success' )
+
 );
 
 --//CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; if uuid doesn't work!
