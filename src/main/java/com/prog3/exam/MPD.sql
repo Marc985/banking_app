a22a13d9267b834e1eb0d0b9d675e131da6e401a
Artiana <hei.rojo.3@gmail.com>
2024-03-13 23:24:42 +0300

    feat:adding queries for database creation

---
+++ src/main/java/com/prog3/exam/MPD.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE account (
+    account_id serial PRIMARY KEY,
+    client_name varchar(150),
+    client_last_name varchar(150),
+    birthdate date,
+    monthly_net_income decimal
+);
+
+
+CREATE TABLE sold (
+    id_sold serial PRIMARY KEY,
+    balance decimal,
+    loans decimal,
+    loansInterest int,
+    "date" date,
+    account_id integer,
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
+
+CREATE TABLE IF NOT EXISTS "transaction" (
+    id_transaction serial PRIMARY KEY,
+    "type" varchar(6) CHECK ("type" IN ('debit', 'credit')),
+    amount decimal,
+    "date" date,
+    reason varchar(20),
+    sender_account_id integer REFERENCES account(account_id),
+    recipient_account_id integer REFERENCES account(account_id)
+);
+
+CREATE TABLE IF NOT EXISTS transfert (
+    reference UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    reason varchar(100),
+    amount decimal,
+    effective_date date,
+    registration_date date,
+    status varchar(7) CHECK (status IN ('cancel', 'pending', 'success'))
+);
