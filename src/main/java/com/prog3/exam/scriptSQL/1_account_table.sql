b5405c57da98c9a9d2cc9c6263250328bc61a899
Marc Herilala <hei.marc.3@gmail.com>
2024-03-27 11:49:39 +0300

    feat(database): add client entity and associate it with many accounts

--- src/main/java/com/prog3/exam/scriptSQL/1_account_table.sql
+++ src/main/java/com/prog3/exam/scriptSQL/1_account_table.sql
@@ -1,13 +1,25 @@
+
 CREATE  TABLE IF NOT EXISTS  account(
-    bigint primary key,
-    client_name varchar(80),
-    client_last_name varchar(80),
-    birthdate date,
-    monthly_net_income double precision,
+    account_number bigint primary key,
     is_eligible boolean default false
     );
 
+CREATE  TABLE client(
+    id_client UUID primary key ,
+    first_name varchar(80),
+    last_name varchar(80),
+    birthdate date,
+    monthly_net_salary double precision
+);
+ALTER TABLE account DROP COLUMN  client_name;
+ALTER TABLE account DROP COLUMN  client_last_name;
+ALTER TABLE account DROP COLUMN  birthdate;
+ALTER TABLE account DROP COLUMN  monthly_net_income;
 
-INSERT INTO account (account_number, client_name, client_last_name, birthdate, monthly_net_income, is_eligible)
-VALUES (123456789, 'John', 'Doe', '1980-01-01', 5000.00, false)
+ALTER TABLE  account ADD COLUMN id_client UUID references client(id_client);
+
+INSERT INTO client values ('550e8400-e29b-41d4-a716-446655440000','John','Doe','2000-04-08',3000)
+    ON CONFLICT (id_client) DO NOTHING;
+INSERT INTO account  (account_number, is_eligible,id_client)
+VALUES (1234567829, false,'550e8400-e29b-41d4-a716-446655440000')
     ON CONFLICT (account_number) DO NOTHING;
