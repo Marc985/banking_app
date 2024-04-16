f18d02e3e9c83651d8a862ed156088aaba43a4dd
Marc Herilala <hei.marc.3@gmail.com>
2024-04-16 12:04:49 +0300

    feat:add category list

--- src/main/java/com/prog3/exam/scriptSQL/3_category_table.sql
+++ src/main/java/com/prog3/exam/scriptSQL/3_category_table.sql
@@ -3,4 +3,17 @@ CREATE TABLE IF NOT EXISTS "category"(
     category_name varchar(100),
     category_type varchar(30) CHECK (category_type='debit' OR category_type='credit')
     );
+INSERT INTO "category" (id_category, category_name, category_type) VALUES
+        (1, 'nourriture et boissons', 'debit'),
+        (2, 'Achats et boutiques en ligne', 'debit'),
+        (3, 'Logement', 'debit'),
+        (4, 'Transports', 'debit'),
+        (5, 'Véhicule', 'debit'),
+        (6, 'Loisirs', 'debit'),
+        (7, 'Multimédia, Informatique', 'debit'),
+        (8, 'Frais financiers', 'debit'),
+        (9, 'Investissements', 'debit'),
+        (10, 'Revenu', 'credit'),
+        (11, 'Autres', 'debit');
+
 
